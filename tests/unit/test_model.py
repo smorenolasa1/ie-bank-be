6e15180d89ae57babfaedfa7ffc4ca1f61a929e8
smorenolasa1 <smoreno.ieu2021@student.ie.edu>
2023-10-15 23:06:43 +0200

    t

--- tests/unit/test_model.py
+++ tests/unit/test_model.py
@@ -1,5 +1,6 @@
 from iebank_api.models import Account
 import pytest
+from iebank_api import db, app
 
 def test_create_account():
     """
@@ -14,4 +15,28 @@ def test_create_account():
     assert account.account_number != None
     assert account.balance == 0.0
     assert account.status == 'Active'
+    db.session.add(account)
+    db.session.commit()
+
+    with app.test_client() as testing_client:
+        yield testing_client
+
+    with app.app_context():
+        db.drop_all()
+
+
+
+def test_account_deposit():
+    account = Account('John Doe', '€', "Spain")
+    initial_balance = account.balance
+    deposit_amount = 100
+    account.deposit(deposit_amount)
+    assert account.balance == initial_balance + deposit_amount
+
+def test_account_withdraw():
+    account = Account('John Doe', '€', "Spain")
+    account.balance = 100  
+    withdrawal_amount = 50
+    account.withdraw(withdrawal_amount)
+    assert account.balance == 50   
 
