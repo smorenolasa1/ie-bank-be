6c92cee73524f3c053c811e7da69e1a435189d3a
smorenolasa1 <smoreno.ieu2021@student.ie.edu>
2023-10-15 22:28:15 +0200

    try

--- tests/functional/test_routes.py
+++ tests/functional/test_routes.py
@@ -26,8 +26,9 @@ def test_create_account(testing_client):
     WHEN the '/accounts' page is posted to (POST)
     THEN check the response is valid
     """
-    response = testing_client.post('/accounts', json={'name': 'John Doe', 'currency': '€', "country": "Spain"})
+    response = testing_client.post('/accounts', json={'name': 'John Doe', 'currency': '€', 'country': 'Spain'})
     assert response.status_code == 200
+
 
 def test_delete_account(testing_client):
     """
@@ -35,16 +36,35 @@ def test_delete_account(testing_client):
     WHEN the '/accounts' page is deleted (DELETE)
     THEN check the response is valid
     """
-    response = testing_client.delete('/accounts/1')
+    response = testing_client.post(
+        "/accounts", json={'name': 'John Doe', 'currency': '€', 'country': 'Spain'}
+    )
+
+    response = testing_client.delete("/accounts/1")
     assert response.status_code == 200
 
-def test_get_account(testing_client):
+def update_account_by_id(testing_client):
+    """
+    GIVEN a Flask application
+    WHEN the '/accounts' page is requested (PUT)
+    THEN check the response is valid
+    """
+    response = testing_client.post(
+        "/accounts", json={'name': 'John Doe', 'currency': '€', 'country': 'Spain'}
+    )
+    response = testing_client.put("/accounts/1", json={"name": "Jane Smith"})
+    assert response.status_code == 200
+
+
+def test_get_account_by_id(testing_client):
     """
     GIVEN a Flask application
     WHEN the '/accounts' page is requested (GET)
     THEN check the response is valid
     """
-    response = testing_client.get('/accounts/1')
+    response = testing_client.post(
+        "/accounts", json={'name': 'John Doe', 'currency': '€', 'country': 'Spain'}
+    )
+
+    response = testing_client.get("/accounts/1")
     assert response.status_code == 200
-
-
--- tests/unit/test_model.py
+++ tests/unit/test_model.py
@@ -14,4 +14,17 @@ def test_create_account():
     assert account.account_number != None
     assert account.balance == 0.0
     assert account.status == 'Active'
-    
+
+def test_account_deposit():
+    account = Account(name="John Doe", currency="€", country="Spain")
+    initial_balance = account.balance
+    deposit_amount = 100
+    account.deposit(deposit_amount)
+    assert account.balance == initial_balance + deposit_amount
+
+def test_account_withdraw():
+    account = Account(name="John Doe", currency="€", country="Spain")
+    account.balance = 200  # Set an initial balance
+    withdrawal_amount = 100
+    account.withdraw(withdrawal_amount)
+    assert account.balance == 100  # The new balance after withdrawal    
